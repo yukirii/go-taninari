0b5835f727f1ae33f86458fec3fde97b8f720eb0
shiftky <shiftky@gmail.com>
2017-07-16 03:37:08 +0900

    Implement cli

---
+++ cmd/taninari/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/shiftky/go-taninari"
+	"github.com/urfave/cli"
+)
+
+const cliVersion = "0.1.0"
+
+func Show(goroku taninari.Goroku) {
+	fmt.Println("たになり語録 - " + goroku.PublishedAt)
+	fmt.Println(goroku.Text)
+	fmt.Println(goroku.PublishedURL)
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "taninari"
+	app.Usage = "人生楽しんでますか？"
+	app.Version = cliVersion
+	app.Action = func(c *cli.Context) error {
+		var goroku taninari.Goroku
+
+		for {
+			goroku = taninari.GetGoroku()
+			if goroku.ImageURL == "" {
+				break
+			}
+		}
+
+		Show(goroku)
+
+		return nil
+	}
+	app.Run(os.Args)
+}
